c0cf666a5738dc0b6fb0b66433cf598e13b4fbf6
Charles Chavis <chavisca@gmail.com>
2023-07-13 20:40:21 -0400

    added API keys to script file with example and added screenshots of api call parameters

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+// OMDB API Key: d9732be9
+// OMDB Data Requests:  http://www.omdbapi.com/?apikey=[yourkey]&
+// OMDB Poster Requests:  http://img.omdbapi.com/?apikey=[yourkey]&
+
+// SERP API Key: 7c6118fdb3f3089d85b5fb869874edfac11e803bd3520e4f5d117c1a8a2e641e
+//SERP API Example
+// https://serpapi.com/search.json?engine=google&q=Coffee
+// params = {
+//     engine: "google",
+//     q: "Coffee",
+//     api_key: "7c6118fdb3f3089d85b5fb869874edfac11e803bd3520e4f5d117c1a8a2e641e"
+//   }
+  
+//   search = GoogleSearch.new(params)
+//   organic_results = search.get_hash[:organic_results]
